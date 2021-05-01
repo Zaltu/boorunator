a055d562a4713fd1b6c036ba95200931ab95f9c9
Zaltu <swwouf@hotmail.com>
2021-05-01 15:33:29 -0400

    working danbooru

--- src/locations/danbooru.py
+++ src/locations/danbooru.py
@@ -5,18 +5,36 @@ import json
 
 import requests
 
-DAN_URL = "https://danbooru.donmai.us/posts.json?limit=1&page=1&tags={tags}"
+# Hee hee
+_RATING_MAP = {
+    "safe": "safe",
+    "questionable": "questionable",
+    "explicit": "explicit"
+}
 
-def danbooru(tags):
-    """
-    Fetch image from danbooru
+RATING_PARAM = "rating:{rating}+"
+DAN_URL = "https://danbooru.donmai.us/posts.json?limit=1&page=1&tags={rating_param}{tags}"
 
-    :param str tags: tags to search for
 
-    :returns: image URL
-    :rtype: str
-    """
-    booru_chan = requests.get(DAN_URL.format(tags=tags)).json()
-    if booru_chan:
-        return booru_chan[0]['file_url']
-    return None
+class danbooru():
+    name = "Danbooru"
+    @staticmethod
+    def search(tags, rating):
+        """
+        Fetch image from danbooru
+
+        :param str tags: tags to search for
+
+        :returns: image URL
+        :rtype: str
+        """
+        url = DAN_URL.format(tags=tags, rating_param=(RATING_PARAM.format(rating=_RATING_MAP[rating]) if rating else ""))
+        dan_res = requests.get(url)
+
+        if not dan_res.ok:
+            dan_res.raise_for_status()
+        dan_json = dan_res.json()
+        if not dan_json:  # No results
+            return None
+        dan_json = dan_json[0]
+        return dan_json.get("file_url")
