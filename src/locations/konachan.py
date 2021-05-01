951c1bfa53e4d7e01df01974a6fdf78a21ae95b8
Zaltu <swwouf@hotmail.com>
2021-05-01 15:15:44 -0400

    working konachan

--- src/locations/konachan.py
+++ src/locations/konachan.py
@@ -5,20 +5,36 @@ import json
 
 import requests
 
-KONA_URL = "https://konachan.com/post.json?page=1&limit=1&tags={tags}"
+# Hee hee
+_RATING_MAP = {
+    "safe": "safe",
+    "questionable": "questionable",
+    "explicit": "explicit"
+}
+
+RATING_PARAM = "rating:{rating}+"
+KONA_URL = "https://konachan.com/post.json?page=1&limit=1&tags={rating_param}{tags}"
 
 
+class konachan():
+    name = "Konachan"
+    @staticmethod
+    def search(tags, rating):
+        """
+        Fetch image from konachan
 
-def konachan(tags):
-    """
-    Fetch image from konachan
+        :param str tags: tags to search for
 
-    :param str tags: tags to search for
+        :returns: image URL
+        :rtype: str
+        """
+        url = KONA_URL.format(tags=tags, rating_param=(RATING_PARAM.format(rating=_RATING_MAP[rating]) if rating else ""))
+        kona_res = requests.get(url)
 
-    :returns: image URL
-    :rtype: str
-    """
-    kona_chan = requests.get(KONA_URL.format(tags=tags)).json()
-    if kona_chan:
-        return kona_chan[0]['file_url']
-    return None
+        if not kona_res.ok:
+            kona_res.raise_for_status()
+        kona_json = kona_res.json()
+        if not kona_json:  # No results
+            return None
+        kona_json = kona_json[0]
+        return kona_json.get("file_url")
