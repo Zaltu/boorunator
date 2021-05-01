07dc42e94693ed043c991b2b063b1a6bb454cf52
Zaltu <swwouf@hotmail.com>
2021-05-01 16:07:16 -0400

    include error handling in example

--- example.py
+++ example.py
@@ -4,5 +4,8 @@ rating = boorunator.ratings.SAFE
 fromsite = boorunator.sites.SANKAKU
 tags = ["elf", "white hair"]
 
-image_url = boorunator.boor(tags, rating=rating)
-print(image_url)
+try:
+    image_url = boorunator.boor(tags, rating=rating)
+    print(image_url)
+except (boorunator.BadTag, boorunator.NoResult) as e:
+    print(str(e) + "\n:(")
